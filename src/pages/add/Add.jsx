eae5082924c19fa7ab6415a3e199d299df302b88
calebthecurious <calebthecurious@gmail.com>
2023-02-26 06:24:11 +1100

    1st commit - project setup

---
+++ src/pages/add/Add.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./add.scss";
+
+const Add = () => {
+  return (
+    <div>
+      <h1>Add</h1>
+    </div>
+  );
+};
+
+export default Add;
---
+++ src/pages/mySkills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./mySkills.scss";
+
+const MySkills = () => {
+  return (
+    <div>
+      <h1>MySkills</h1>
+    </div>
+  );
+};
+
+export default MySkills;
