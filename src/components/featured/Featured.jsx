709135cc6a4c05abeaf50213df87aa4d0ed28a40
calebthecurious <calebthecurious@gmail.com>
2023-02-27 11:41:52 +1100

    4th commit - featured component design

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Featured.scss";
+import { useNavigate } from "react-router-dom";
+
+function Featured() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate(`/skills?search=${input}`);
+  };
+  return (
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          <h1>
+            Find the perfect <span>neighbour</span> to guide you!
+          </h1>
+          <div className="search">
+            <div className="searchInput">
+              <img src="./img/search.png" alt="" />
+              <input
+                type="text"
+                placeholder='Try "building mobil app"'
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </div>
+            <button onClick={handleSubmit}>Search</button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Learn Guitar</button>
+            <button>Spanish Lessons</button>
+            <button>Shopify Website</button>
+            <button>Growing Tomatoes</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src="./img/mman.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
 import "./home.scss";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <Featured />
     </div>
   );
 };
