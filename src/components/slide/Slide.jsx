e4d7b4bb8067ab4fc21866437a052822b0413bfa
calebthecurious <calebthecurious@gmail.com>
2023-02-27 12:31:43 +1100

    6th commit - category slider added

---
+++ src/components/slide/Slide.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Slider from "infinite-react-carousel";
+import CatCard from "../catCard/CatCard";
+import { cards } from "../../data";
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <div className="container">
+        <Slider slidesToShow={5} arrowsScroll={2}>
+          {cards.map((card) => (
+            <CatCard item={card} key={card.id} />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
+import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./home.scss";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={2}>
+        {cards.map((card) => (
+          <CatCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
